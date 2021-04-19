41ced732fd3e994d9fbfe60ec25d3e1feddcb2a5
joondae <68300257+joondae@users.noreply.github.com>
2021-04-18 23:34:16 -0700

    Coded closeToBoundary assignment
    New: method closeToBoundaryA + test code + txt file with sample inputs
    
    Updated: comments, the way txt files are written

--- SquishMethod.java
+++ SquishMethod.java
@@ -4,10 +4,12 @@ import java.util.HashSet;
 import java.util.Set;
 */
 
+//Change class name
 public class SquishMethod {
 	/**
+	 * Option A:
 	 * Takes in a single number as an input between -100 and 100
-	 * Outputs a number between 0 and 1  relative to input number size
+	 * Outputs a number between 0 and 1 relative to input number size
 	 * Condition 1: given two inputs iA and iB where iA > iB, output oA must be greater than oB for all inputs
 	 * Condition 2: a specific input always calculates the same output
 	 * Condition 3: every unique input has a unique output
@@ -22,6 +24,52 @@ public class SquishMethod {
 		
 		//Output determined by Sigmoid function
 		output = 1/(1 + Math.exp(-1 * input));
+		
+		return output;
+	}
+	
+	/**
+	 * Option A continued:
+	 * Takes in a single number as an input between 0 and 1
+	 * Outputs a number between 0.25 and 0
+	 * Condition 1: output must be greatest when input is 0.5 and smallest when input is 0 or 1
+	 * Condition 2: any inputs < 0.5 produce decreasing outputs as the inputs decrease from 0.5 to 0
+	 * Condition 3: any inputs > 0.5 produce decreasing outputs as the inputs increase from 0.5 to 1
+	 * Condition 4: a specific input always calculates the same output
+	 * Condition 5: every unique input has a unique output
+	 */
+	public static double closeToBoundaryA(double input) {
+		double output = -1;
+		
+		//If input isn't within acceptable range, return -1 (an invalid output)
+		if(input >= 1 || input <= 0) {
+			return output;
+		}
+		
+		//Output determined by piecewise function
+		if(input < 0.5) {
+			if(input < 0.4) {
+				//For 0 ≤ input ≤ 0.4, y = -0.9375(x - 0.4)^2 + 0.15
+				output = -0.9375 * Math.pow(input - 0.4, 2) + 0.15;
+			}
+			else if(input == 0.4) {
+				//For x = 0.4, y = 0.15
+				//Arbitrarily chosen "pivot point" to make the decision boundary function more interesting?
+				output = 0.15;
+			}
+			else if(input > 0.4) {
+				//For 0.4 ≤ input ≤ 0.5, y = -10(x - 0.5)^2 + 0.25
+				output = -10 * Math.pow(input - 0.5, 2) + 0.25;
+			}
+		}
+		else if(input == 0.5) {
+			//For x = 0.5, y = 0.25
+			output = 0.25;
+		}
+		else if(input > 0.5){
+			//For 1 ≥ x ≥ 0.5, y = -x^2 + x
+			output = (-1 * Math.pow(input, 2)) + (input);
+		}
 		
 		return output;
 	}
--- SquishMethodTester.java
+++ SquishMethodTester.java
@@ -2,6 +2,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
+//Change class name
 public class SquishMethodTester {
 	public static void main(String [] args) {
 		//Testing assignment sample inputs
@@ -9,16 +10,36 @@ public class SquishMethodTester {
 		System.out.println("Running sample inputs...");
 		try {
 			BufferedWriter bw = new BufferedWriter(new FileWriter("SquishA.txt"));
-			bw.write("inputs: [-1337.1337, -10.5, -5.5, -1.5, -1, -0.3, -0.113, 0, 0.31, 0.76, 1.3, 1.99, 7.4, 160, 1337]\n");
-			double [] inputs = new double[] {-1337.1337, -10.5, -5.5, -1.5, -1, -0.3, -0.113, 0, 0.31, 0.76, 1.3, 1.99, 7.4, 160, 1337};
-			for(int i = 0; i < inputs.length; i++) {
-				bw.write("\ninput: " + inputs[i] + "  |  output: " + SquishMethod.squishA(inputs[i]));
+			double[] squishInputs = new double[] {-1337.1337, -10.5, -5.5, -1.5, -1, -0.3, -0.113, 0, 0.31, 0.76, 1.3, 1.99, 7.4, 160, 1337};
+			double[] squishOutputs = new double[squishInputs.length];
+			bw.write("Squish function (A):\n");
+			bw.write("inputs (" + squishInputs.length + "): [-1337.1337, -10.5, -5.5, -1.5, -1, -0.3, -0.113, 0, 0.31, 0.76, 1.3, 1.99, 7.4, 160, 1337]\n");
+			for(int i = 0; i < squishInputs.length; i++) {
+				double output = SquishMethod.squishA(squishInputs[i]);
+				squishOutputs[i] = output;
+				bw.write("\ninput: " + squishInputs[i] + "  |  output: " + output);
 			}
 			bw.close();
+			System.out.println("\tDone running squish function");
+			
+			BufferedWriter bw2 = new BufferedWriter(new FileWriter("CloseToBoundaryA.txt"));
+			double[] boundaryInputs = new double[] {0, 0.000027535691114583473, 0.004070137715896128, 0.18242552380635635, 0.2689414213699951, 0.425557483188341, 0.47178002201963243, 0.5, 0.5768852611320463, 0.6813537337890256, 0.7858349830425586, 0.8797431375322491, 0.9993891206405656, 1, 1};
+			double[] boundaryOutputs = new double[boundaryInputs.length];
+			bw2.write("Close to boundary function (A):\n");
+			bw2.write("inputs (" + boundaryInputs.length + "): [0, 0.000027535691114583473, 0.004070137715896128, 0.18242552380635635, 0.2689414213699951, 0.425557483188341, 0.47178002201963243, 0.5, 0.5768852611320463, 0.6813537337890256, 0.7858349830425586, 0.8797431375322491, 0.9993891206405656, 1, 1]\n");
+			for(int i = 0; i < squishInputs.length; i++) {
+				double output = SquishMethod.closeToBoundaryA(boundaryInputs[i]);
+				boundaryOutputs[i] = output;
+				bw2.write("\ninput: " + boundaryInputs[i] + "  |  output: " + output);
+			}
+			bw2.close();
+			System.out.println("\tDone running close to boundary function");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println("Done");
+		
+		
+		System.out.println("Done running sample inputs");
 		
 		//IGNORE - USED FOR TESTING
 		/*
